1a21b765772dd2f8fb026111ba94a00e2e1a25d7
Jonathan Melly <jonathan.melly@gmail.com>
2024-11-27 14:45:52 +0100

    feat(rename-copy): copy assignment with good name... aka moodle style

---
+++ rename-teams-assignments.ps1
@@ -0,0 +1,34 @@
+#usage .\rename-teams-assignments.ps1 -in C:\...\Assignments -filter test1 -out C:\temp\copiedAndRenamedZips
+param(
+  [Parameter(Mandatory=$true)]
+  [string]$in,
+
+  [Parameter(Mandatory=$true)]
+  [string]$filter,
+
+  [Parameter(Mandatory=$true)]
+  [string]$out
+)
+
+
+$students = Get-ChildItem -Path $in -Directory
+
+Write-Debug "Found students: $students"
+
+foreach ($subdirectory in $students) {
+
+  Write-Debug "Analyzing $subdirectory"
+
+  $zipFiles = Get-ChildItem -Path $subdirectory.FullName -Filter "*.zip" -Recurse -File | Where-Object { $_.FullName -like "*$filter*" }
+
+  foreach ($zipFile in $zipFiles) {
+      
+      $newFileName = "$subdirectory.zip"
+      $inPath = "$($zipFile.FullName)"
+      $outPath = Join-Path -Path "$out" -ChildPath "$newFileName"
+
+      Write-Debug "Found $zipFile, about to copy $inPath->$outPath"
+
+      Copy-Item -Path "$inPath" -Destination "$outPath"
+  }
+}
